58ed6625ff154a096d36a8ded6929ff6ef705302
Janos Perge <janos.perge@gmail.com>
2022-07-14 20:53:41 +0000

    use index.html

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 let express = require('express');
 let app = express();
-
+/*
 app.get("/", function(req, res) {
   res.send("Hello Express");
 });
+*/
 
+app.get("/", function (req, res){
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 
 
