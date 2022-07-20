51c7170c5298350a42d068ac8cb294dedb54598a
Janos Perge <janos.perge@gmail.com>
2022-07-20 19:31:27 +0000

    add assets route

--- myApp.js
+++ myApp.js
@@ -10,6 +10,9 @@ app.get("/", function (req, res){
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 
 
 
