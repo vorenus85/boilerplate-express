844605de35faddf60123bf993cb534445ce4a6cf
Janos Perge <janos.perge@gmail.com>
2022-07-20 19:47:13 +0000

    fix json response

--- myApp.js
+++ myApp.js
@@ -13,8 +13,8 @@ app.get("/", function (req, res){
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", function (req, res) {
-  res.json({"message": "Hello json"})
+app.get("/json", function (req, res){
+  res.json({message: "Hello json"});
 });
 
 
